ca66dd7c9e4a4461965db71b049f2b888956a009
MirzadS <mirzad.smoloo@gmail.com>
2023-01-25 00:39:32 +0100

    Register screen - Date Picker

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:praksa_frontend/ui/forms/register_form.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'ZEV',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Home Page'),
     );
   }
 }
@@ -29,14 +31,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,20 +40,18 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          // children: [],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              // builder: (context) => const RegisterForm(),
+              builder: (context) => const RegisterForm(),
+            ),
+          );
+        },
         child: const Icon(Icons.add),
       ),
     );
---
+++ lib/ui/forms/register_form.dart
@@ -0,0 +1,197 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:praksa_frontend/ui/Helper/data_picker.dart';
+
+import '../background/background.dart';
+
+class RegisterForm extends StatefulWidget {
+  const RegisterForm({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<RegisterForm> {
+  TextEditingController _date = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(children: [
+        Background(),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Background(),
+            Container(
+              margin: const EdgeInsets.only(bottom: 55),
+              child: const Text(
+                "Register",
+                style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 350,
+              child: Stack(
+                children: [
+                  Container(
+                    // height: 150,
+                    margin: const EdgeInsets.only(right: 70),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: const BorderRadius.only(
+                        topRight: Radius.circular(100),
+                        bottomRight: Radius.circular(100),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 0,
+                          blurRadius: 10,
+                          offset: const Offset(0, 4),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(left: 16, right: 32),
+                          child: const TextField(
+                            decoration: InputDecoration(
+                              hintStyle: TextStyle(fontSize: 16),
+                              border: InputBorder.none,
+                              icon: Icon(Icons.account_circle_rounded),
+                              hintText: "First Name",
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(left: 16, right: 32),
+                          child: const TextField(
+                            decoration: InputDecoration(
+                              hintStyle: TextStyle(fontSize: 16),
+                              border: InputBorder.none,
+                              icon: Icon(Icons.account_circle_rounded),
+                              hintText: "Last Name",
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(left: 16, right: 32),
+                          child: const TextField(
+                            decoration: InputDecoration(
+                              hintStyle: TextStyle(fontSize: 16),
+                              border: InputBorder.none,
+                              icon: Icon(Icons.person),
+                              hintText: "Username",
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(left: 16, right: 32),
+                          child: const TextField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              hintStyle: TextStyle(fontSize: 16),
+                              border: InputBorder.none,
+                              icon: Icon(Icons.password),
+                              hintText: "Password",
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(left: 16, right: 32),
+                          child: TextField(
+                              controller: _date,
+                              // obscureText: true,
+                              decoration: const InputDecoration(
+                                hintStyle: TextStyle(fontSize: 16),
+                                border: InputBorder.none,
+                                icon: Icon(Icons.date_range),
+                                hintText: "Select Date",
+                              ),
+                              readOnly: true,
+                              onTap: () async {
+                                DateTime? pickedDate = await showDatePicker(
+                                    context: context,
+                                    initialDate: DateTime.now(),
+                                    firstDate: DateTime(2000),
+                                    lastDate: DateTime(2101));
+
+                                if (pickedDate != null) {
+                                  setState(() {
+                                    _date.text = DateFormat('yyyy-MM-dd')
+                                        .format(pickedDate);
+                                  });
+                                }
+                              }),
+                        ),
+                        // DatePicker(date: _date)
+                      ],
+                    ),
+                  ),
+
+                  // KRUG
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Container(
+                      margin: const EdgeInsets.only(right: 15),
+                      height: 80,
+                      width: 80,
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.green[200]!.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
+                        shape: BoxShape.circle,
+                        gradient: const LinearGradient(
+                          begin: Alignment.centerLeft,
+                          end: Alignment.centerRight,
+                          colors: [
+                            Color(0xff1bccba),
+                            Color(0xff22e2ab),
+                          ],
+                        ),
+                      ),
+                      child: const Icon(
+                        Icons.arrow_forward_outlined,
+                        color: Colors.white,
+                        size: 32,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(left: 16, top: 24),
+                  padding: const EdgeInsets.only(top: 24),
+                  child: const Text(
+                    "Login",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0xffe98f60),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ]),
+    );
+  }
+}
