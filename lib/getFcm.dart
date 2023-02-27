bfbbd986c78f0345459cb820307074040624e41c
unknown <eldar.sose@edu.fit.ba>
2023-02-27 10:44:05 +0100

    change branch

--- lib/getFcm.dart
+++ lib/getFcm.dart
@@ -1,8 +1,12 @@
 import 'dart:io';
-
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 Future<String?> getFcmToken() async {
+  if(kIsWeb){
+    String? fcmKey = await FirebaseMessaging.instance.getToken();
+    return fcmKey;
+  }
   if (Platform.isIOS) {
     String? fcmKey = await FirebaseMessaging.instance.getToken();
     return fcmKey;
--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,15 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:praksa_frontend/ForGroundLocalNotification.dart';
+import 'package:praksa_frontend/ui/firebase_options.dart';
 import 'package:praksa_frontend/ui/forms/login_form.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await Hive.initFlutter();
 
   var box = await Hive.openBox("mybox");
